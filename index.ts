bc99ad229da511a4ff4dda45d215cb2d72132912
Chu Ngoc Thang <chuthanglsz@gmail.com>
2024-04-14 11:40:16 +0700

    connect database

--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
 import express, {Express,Request,Response} from "express";
+import moment from "moment";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import sequelize from "./config/database";
+dotenv.config();
+//Conect database
+sequelize;
 
 const app:Express = express();
-const port:number=3000;
+const port:number|string=process.env.PORT||3000;
 
-app.set("views", `${__dirname}/views`)
-app.set('view engine', 'pug')
+app.set("views", `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+app.use(express.static("public"));
+//bodyParser
+app.use(bodyParser.json());
+//End bodyParser
+
+//APP LOCAL VARIABLE
+app.locals.moment=moment;
+//END APP LOCAL VARIABLE
 
 app.get("/tours",(req:Request,res:Response) =>{
   res.render("client/pages/tours/index");
